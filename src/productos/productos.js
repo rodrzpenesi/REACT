5c9fe015646dc13f9d864646b91c7eb627f10f35
Rodrigo Penesi <rodrz.penesi@gmail.com>
2023-06-06 14:06:12 -0300

    fix2da

--- src/productos/productos.js
+++ src/productos/productos.js
@@ -2,5 +2,10 @@ const productos = [
 { id:1, nombre: "Rapiditas Clasicas", imagen: "/src/assets/rapiditas.png", precio: 370 ,categoria: "Bimbo"},
 { id:2, nombre: "Rapiditas Light", imagen: "/src/assets/rapiditasLight.png", precio: 370 ,categoria: "Bimbo"},
 { id:3, nombre: "Rapiditas Integrales", imagen: "/src/assets/rapiditasIntregrales.png", precio: 370 ,categoria: "Bimbo"},
+{ id:4, nombre: "Artesano ", imagen: "/src/assets/artesano.png", precio: 770 ,categoria: "Bimbo"},
+{ id:5, nombre: "Artesano Semillas de Campo", imagen: "/src/assets/artesanoSemillas.png", precio: 770 ,categoria: "Bimbo"},
+{ id:6, nombre: "Blanco 450gr", imagen: "/src/assets/blanco.png", precio: 550 ,categoria: "Bimbo"},
+{ id:7, nombre: "Blanco 550gr", imagen: "/src/assets/blancoGrande.png", precio: 650 ,categoria: "Bimbo"},
+{ id:8, nombre: "Integral", imagen: "/src/assets/integral.png", precio: 600 ,categoria: "Bimbo"},
 ]
 export default productos;
